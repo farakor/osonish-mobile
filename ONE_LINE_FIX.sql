019ace4957c17b460cd219229a0115638ad13373
Farrukh Oripov <farrukh.oripov@gmail.com>
2025-11-29 16:06:37 +0500

    Clean up: remove old documentation, SQL scripts and test files

--- ONE_LINE_FIX.sql
+++
@@ -1,4 +0,0 @@
--- 🎯 ОДНА КОМАНДА для решения проблемы "bucket не найден"
--- Скопируйте и выполните в SQL Editor:
-
-CREATE POLICY "Allow everyone to see buckets" ON storage.buckets FOR SELECT USING (true); 
